786adbc0c12507cad722d0b7ee300d87264e2f39
XateroX <dan.lostarkgames@gmail.com>
2023-11-04 20:40:24 +0000

    added: main beginning, assigns to all creeps their role in the mapping and to all spawners the role of spawner

---
+++ main.js
@@ -0,0 +1,21 @@
+// This file is used in my account of the online MMO Screeps
+
+var roleHarvester = require('role.harvester');
+var roleSpawner = require('role.spawner');
+
+creepRoleMapping = {
+    'harvester': roleHarvester
+}
+
+module.exports.loop = function () {
+    // set all spawners to use the roleSpawner module
+    for (var name in Game.spawns) {
+        roleSpawner.run(Game.spawns[name]);
+    }
+
+    // set all creeps to use the role they are assigned
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        creepRoleMapping[creep.memory.role].run(creep);
+    }
+}
