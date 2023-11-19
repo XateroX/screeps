a2ac72c088f594f5ab27682b853b30e5c6e9872c
XateroX <dan.lostarkgames@gmail.com>
2023-11-19 15:52:44 +0000

    Add room defense logic to main loop

--- main.js
+++ main.js
@@ -26,4 +26,19 @@ module.exports.loop = function () {
         var creep = Game.creeps[name];
         creepRoleMapping[creep.memory.role].run(creep);
     }
+
+    for (let spawn in Game.spawns) {
+        defendRoom(Game.spawns[spawn].room.name);
+    }
+}
+
+function defendRoom(roomName) {
+    var hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
+    if (hostiles.length > 0) {
+        var username = hostiles[0].owner.username;
+        Game.notify(`User ${username} spotted in room ${roomName}`);
+        var towers = Game.rooms[roomName].find(
+            FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
+        towers.forEach(tower => tower.attack(hostiles[0]));
+    }
 }
