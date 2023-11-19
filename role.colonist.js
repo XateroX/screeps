e84a0e05108954e9bf23dc3f6c9b5477aaeeb4f5
XateroX <dan.lostarkgames@gmail.com>
2023-11-19 14:44:42 +0000

    Fix findRoute result handling

--- role.colonist.js
+++ role.colonist.js
@@ -8,7 +8,11 @@ const roleColonist = {
         if (creep.room.name != creep.memory.targetRoom) {
             // using Game.map.findRoute() to get list of exits to go through to get to target room (only call this if creep.memory.movements is empty)
             if (!creep.memory.movements || creep.memory.movements.length == 0) {
-                creep.memory.movements = Game.map.findRoute(creep.room.name, creep.memory.targetRoom);
+                let result = Game.map.findRoute(creep.room.name, creep.memory.targetRoom);
+                console.log("result of findRoute: " + result);
+                if (result != -2) {
+                    creep.memory.movements = result;
+                }
             }
 
             if (creep.room.name != creep.memory.movements[0].room) {
