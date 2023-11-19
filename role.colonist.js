34e595024bf717fde194b09f13b6207417a5309a
XateroX <dan.lostarkgames@gmail.com>
2023-11-19 14:37:45 +0000

    Fix target room assignment in roleColonist

--- role.colonist.js
+++ role.colonist.js
@@ -1,17 +1,17 @@
 
 const roleColonist = {
     run: function (creep) {
-        if (!creep.memory.targetRoom || creep.memory.targetRoom === 'NONE') {
-            creep.memory.targetRoom = creep.memory.homeRoom;
+        if (!creep.memory.targetRoom || creep.memory.targetRoom == 'NONE') {
+            creep.memory.targetRoom = creep.room.name;
         }
 
-        if (creep.room.name !== creep.memory.targetRoom) {
+        if (creep.room.name != creep.memory.targetRoom) {
             // using Game.map.findRoute() to get list of exits to go through to get to target room (only call this if creep.memory.movements is empty)
             if (!creep.memory.movements || creep.memory.movements.length === 0) {
                 creep.memory.movements = Game.map.findRoute(creep.room, creep.memory.targetRoom);
             }
 
-            if (creep.room.name !== creep.memory.movements[0].room) {
+            if (creep.room.name != creep.memory.movements[0].room) {
                 // remove the first element
                 creep.memory.movements.shift();
             }
